6f92a4159835aef13025e7f455a302d1223effe1
williamsd3 <williamsd@ubuntu.ubuntu-domain>
2014-08-28 12:49:06 -0400

    gitignore among other things

--- MIPS/asm/main.c
+++ MIPS/asm/main.c
@@ -11,12 +11,14 @@ typedef struct instruction {
 } instruction_t;
 
 int main(int argc, char* argv[]) {
+/*
 	if(argc < 3) {
-		printf("Usage: %s input_file output_file", argv[0]);
+		printf("Usage: %s input_file output_file\n", argv[0]);
+		return 1;
 	}
 	char* inFile = argv[1];
 	char* outFile = argv[2];
-	
+*/	
 	FILE* lut = fopen("instruction.hex", "rb");
 	instruction_t instructions[NUM_INSTR];
 	for(int i = 0; i < NUM_INSTR; i++) {
@@ -24,10 +26,10 @@ int main(int argc, char* argv[]) {
 		instructions[i].type = fgetc(lut);
 		instructions[i].opcode = fgetc(lut);
 		instructions[i].funct = fgetc(lut);
-		//printf("%s, %d, %d, %d\n", instructions[i].name, (int)instructions[i].type, (int)instructions[i].opcode, (int)instructions[i].funct);
+		printf("%s, %d, %d, %d\n", instructions[i].name, (int)instructions[i].type, (int)instructions[i].opcode, (int)instructions[i].funct);
 	}
 	fclose(lut);
-	
+/*	
 	FILE* inHandle = fopen(inFile, "r");
 	FILE* outHandle = fopen(outFile, "wb");
 	
@@ -35,4 +37,6 @@ int main(int argc, char* argv[]) {
 	
 	fclose(inHandle);
 	fclose(outHandle);
+*/
+	return 0;
 }
