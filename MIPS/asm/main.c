ec630c57079f1a63b17026fe97f2c335d8cb41a3
Dakota Williams <drw9888@rit.edu>
2014-08-28 17:22:15 -0400

    added register translation

--- MIPS/asm/main.c
+++ MIPS/asm/main.c
@@ -2,13 +2,19 @@
 
 #define INSTR_LEN 6
 #define NUM_INSTR 42
+#define NUM_REG 30
 
 typedef struct instruction {
 	char name[INSTR_LEN];
-	unsigned char type;
+	char type;
 	unsigned char opcode;
 	unsigned char funct;
 } instruction_t;
+
+typedef struct register_ {
+	char name[INSTR_LEN];
+	unsigned char reg;
+} register_t;
 
 int main(int argc, char* argv[]) {
 /*
@@ -18,7 +24,7 @@ int main(int argc, char* argv[]) {
 	}
 	char* inFile = argv[1];
 	char* outFile = argv[2];
-*/	
+*/
 	FILE* lut = fopen("instruction.hex", "rb");
 	instruction_t instructions[NUM_INSTR];
 	for(int i = 0; i < NUM_INSTR; i++) {
@@ -26,9 +32,15 @@ int main(int argc, char* argv[]) {
 		instructions[i].type = fgetc(lut);
 		instructions[i].opcode = fgetc(lut);
 		instructions[i].funct = fgetc(lut);
-		printf("%s, %d, %d, %d\n", instructions[i].name, (int)instructions[i].type, (int)instructions[i].opcode, (int)instructions[i].funct);
+		printf("%5s, %c, %d, %d\n", instructions[i].name, (int)instructions[i].type, (int)instructions[i].opcode, (int)instructions[i].funct);
 	}
 	fclose(lut);
+
+	FILE* reg = fopen("register.hex", "rb");
+	register_t registers[NUM_REG];
+	for(int j = 0; j < NUM_REG; j++) {
+		
+	}
 /*	
 	FILE* inHandle = fopen(inFile, "r");
 	FILE* outHandle = fopen(outFile, "wb");
