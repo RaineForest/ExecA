d78e411b020f2b0f37a9a6a452791872802bbd20
Dakota Williams <drw9888@rit.edu>
2014-08-28 21:40:51 -0400

    Argument parsing works

--- MIPS/asm/main.c
+++ MIPS/asm/main.c
@@ -1,3 +1,6 @@
+
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include "dlList.h"
 #include <stdlib.h>
@@ -7,38 +10,39 @@
 #define REG_LEN 6
 #define NUM_INSTR 42
 #define NUM_REG 32
-#define TOKENS " ,()"
+#define TOKENS " ,()\n"
+#define MAX_LINE 80
 
 typedef struct instruction {
 	char name[INSTR_LEN];
 	char type;
 	unsigned char opcode;
 	unsigned char funct;
-} instruction_t;
+} instruction_T;
 
 typedef struct register_ {
 	char name[INSTR_LEN];
 	unsigned char reg;
-} register_t;
+} register_T;
 
 struct label {
 	char* labelname;
 	unsigned short address;
-}
+};
 
 //sequestial search, cuz not sorted
-int findInstr(instruction_t[] arr, int size, char* string, int len) {
+int findInstr(instruction_T arr[], int size, char* string, int len) {
 	for(int i = 0; i < size; i++) {
-		if(strncmp(string, len, arr.name) == 0) {
+		if(strncmp(string, arr[i].name, len) == 0) {
 			return i;
 		}
 	}
 	return -1;
 }
 
-DlList getParams(char* str) {
-	DlList params;
-	dll_create(params);
+DlList_T getParams(char* str) {
+	DlList_T params;
+	params = dll_create();
 	char* part = strtok(str, TOKENS);
 	while(part != NULL) {
 		dll_append(params, part);
@@ -56,7 +60,7 @@ int main(int argc, char* argv[]) {
 	char* outFile = argv[2];
 
 	FILE* lut = fopen("instruction.hex", "rb");
-	instruction_t instructions[NUM_INSTR];
+	instruction_T instructions[NUM_INSTR];
 	for(int i = 0; i < NUM_INSTR; i++) {
 		fgets(instructions[i].name, INSTR_LEN, lut);
 		instructions[i].type = fgetc(lut);
@@ -67,7 +71,7 @@ int main(int argc, char* argv[]) {
 	fclose(lut);
 
 	FILE* reg = fopen("register.hex", "rb");
-	register_t registers[NUM_REG];
+	register_T registers[NUM_REG];
 	for(int j = 0; j < NUM_REG; j++) {
 		fgets(registers[j].name, REG_LEN, reg);
 		registers[j].reg = fgetc(reg);
@@ -76,25 +80,36 @@ int main(int argc, char* argv[]) {
 	}
 	fclose(reg);
 
-	DlList labels;
-	dll_create(labels);
+	DlList_T labels = dll_create();
 	FILE* inHandle = fopen(inFile, "r");
 	FILE* outHandle = fopen(outFile, "wb");
 	
 	for(int i = 0; !feof(inHandle); i+=4) {
-		unsigned short binaryinterp = 0;
-		int linelen;
-		char* line;
-		getline(&line, &linelen, inHandle);
+		unsigned int binaryinterp = 0;
+		unsigned int linelen = MAX_LINE;
+		char* line = (char*)malloc(MAX_LINE);
+		int ret = getline(&line, &linelen, inHandle);
+		if (ret == -1) {
+			free(line);
+			break;
+		}
+		printf("%s\n", line);
 		
-		DlList params = getParams(line);
+		DlList_T params = getParams(line);
 
+		int size = dll_size(params);
+		for(int j = 0; j < size; j++) {
+			char* stuff = (char*)dll_get(params, j);
+			printf("%s\n", stuff);
+		}
+/*
 		int index = 0;
 		if(index = findInstr(instructions, NUM_INSTR, dll_get(params, 0)) >= 0) {
 			binaryinterp = instructions[index].opcode;
 			binaryinterp << 26;
 			switch(instructions[index].type) {
 				case 'R':
+					
 					break;
 				case 'I':
 					break;
@@ -103,8 +118,9 @@ int main(int argc, char* argv[]) {
 				default:
 			}
 		}
-
+*/
 		dll_destroy(params);
+		free(line);
 	}
 	
 	fclose(inHandle);
