29f482a52231f41dea2c8801115d98a7d0b3d691
Dakota Williams <dwms9395@verizon.net>
2016-06-14 16:37:38 -0400

    changes to elftest

--- elftest.cpp
+++ elftest.cpp
@@ -2,11 +2,16 @@
 #include "elf.h"
 #include <cstdio>
 
-int main() {
+int main(int argc, char** argv) {
+	if(argc != 2) {
+		printf("Usage: elftest <elf file>\n");
+		return 1;
+	}
+
 	ELF* e = new ELF();
 
 	try {
-		e->read("filename");
+		e->read(argv[1]);
 	} catch(string* str) {
 		printf("Error: %s\n", str->c_str());
 		delete e;
